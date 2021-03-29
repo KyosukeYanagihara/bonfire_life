bfb299914455bd81fed2ce332420dea53101406b
KyosukeYanagihara <rctttzg7@gmail.com>
2021-03-29 10:53:39 +0900

    add:address_model作成

---
+++ db/migrate/20210329015039_create_addresses.rb
@@ -0,0 +1,14 @@
+class CreateAddresses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :addresses do |t|
+      t.integer :zipcode, null: false
+      t.string :prefecture, null: false
+      t.string :city, null: false
+      t.string :address1
+      t.string :address2
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
