2622624e27d41aaa03e457b8a1fdb7f2e4203b94
KyosukeYanagihara <rctttzg7@gmail.com>
2021-03-31 15:39:53 +0900

    add:photo_model作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_29_064040) do
+ActiveRecord::Schema.define(version: 2021_03_30_052657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,14 @@ ActiveRecord::Schema.define(version: 2021_03_29_064040) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.text "image", null: false
+    t.bigint "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_photos_on_product_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -75,4 +83,5 @@ ActiveRecord::Schema.define(version: 2021_03_29_064040) do
 
   add_foreign_key "accounts", "users"
   add_foreign_key "addresses", "users"
+  add_foreign_key "photos", "products"
 end
