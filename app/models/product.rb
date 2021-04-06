2dfcc81fabde0df89222a43d10fca7e03387067f
KyosukeYanagihara <rctttzg7@gmail.com>
2021-04-06 17:42:44 +0900

    add:アソシエーション設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   has_many :photos
   has_many :favorites, dependent: :destroy
+  has_many :reviews, dependent: :destroy
   accepts_nested_attributes_for :photos
 end
