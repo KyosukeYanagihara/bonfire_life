1d09fa8a4c12a36c34d30dfccb7dc8e758441d9f
KyosukeYanagihara <rctttzg7@gmail.com>
2021-04-07 13:46:01 +0900

    add:レビュー編集機能追加

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,6 @@
 class ReviewsController < ApplicationController
+  before_action :set_product
   def create
-    @product = Product.find(params[:product_id])
     @review = @product.reviews.build(review_params)
     @review.user_id = current_user.id
     respond_to do |format|
@@ -12,9 +12,34 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def edit
+    @review = @product.reviews.find(params[:id])
+    respond_to do |format|
+      flash.now[:notice] = 'レビューの編集中'
+      format.js { render :edit }
+    end
+  end
+
+  def update
+    @review = @product.reviews.find(params[:id])
+      respond_to do |format|
+        if @review.update(review_params)
+          flash.now[:notice] = 'レビューが編集されました'
+          format.js { render :index }
+        else
+          flash.now[:notice] = 'レビューの編集に失敗しました'
+          format.js { render :edit_error }
+        end
+      end
+  end
+
   private
 
   def review_params
     params.require(:review).permit(:product_id, :comment)
+  end
+
+  def set_product
+    @product = Product.find(params[:product_id])
   end
 end
