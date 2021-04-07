fd9aa032e2018874852646b99f3c0686e20bfd64
KyosukeYanagihara <rctttzg7@gmail.com>
2021-04-07 12:53:07 +0900

    add:レビュー一覧表示、投稿機能追加

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,20 @@
+class ReviewsController < ApplicationController
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.build(review_params)
+    @review.user_id = current_user.id
+    respond_to do |format|
+      if @review.save
+        format.html { redirect_to product_path(@product) }
+      else
+        format.html { redirect_to product_path(@product), notice: '投稿できませんでした...' }
+      end
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:product_id, :comment)
+  end
+end
