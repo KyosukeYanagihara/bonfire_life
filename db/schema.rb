71931a9af55633ee6c24c5c30b1e61dc5559b0d9
KyosukeYanagihara <rctttzg7@gmail.com>
2021-04-11 14:05:04 +0900

    add:注文情報に合計金額を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_09_051605) do
+ActiveRecord::Schema.define(version: 2021_04_11_045633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 2021_04_09_051605) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "price", default: 0, null: false
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
