77be814f8781051260e8b9967c7ad860daaddecf
KyosukeYanagihara <rctttzg7@gmail.com>
2021-04-15 08:40:09 +0900

    add:i18n辞書機能追加

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module BonfireLife
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
     config.active_record.default_timezone = :local
     config.generators do |g|
       g.stylesheets false
