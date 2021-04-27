b1183e2f7eae7c77002127231354d12db52434b5
KyosukeYanagihara <rctttzg7@gmail.com>
2021-04-27 11:06:01 +0900

    remove:turbolinksオフ

--- app/assets/javascripts/show_product.js
+++ app/assets/javascripts/show_product.js
@@ -1,4 +1,4 @@
-$(document).on('turbolinks:load', function() {
+$(function() {
     $('.image_list').click(function () {
       var $src = $(this).attr('src');
       $('.main_image').attr('src', $src);
